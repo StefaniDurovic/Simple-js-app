c4cdf5f87ce4139386020dfe9c049b9af0922677
StefaniDurovic <95530637+StefaniDurovic@users.noreply.github.com>
2022-09-21 17:04:29 +0200

    added inital elements to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood='lasagna';
-document.write(favoriteFood);
+let  pokemonList = [
+    {name:'Pidgeotto', height:'1.1', types:['flying','normal']},
+    {name:'Venomoth', height:'1.5', types:['bug','poison']},
+    {name:'Sandshrew', height:'0.6', types:['ground']}
+];
