c88355641e2ebaf93d53152a0cf12226cb1d682a
StefaniDurovic <95530637+StefaniDurovic@users.noreply.github.com>
2022-09-25 02:28:09 +0200

    created a for loop for object iteration

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+// array of objects
 let  pokemonList = [
-    {name:'Pidgeotto', height:'1.1', types:['flying','normal']},
-    {name:'Venomoth', height:'1.5', types:['bug','poison']},
-    {name:'Sandshrew', height:'0.6', types:['ground']}
+        {name:'Pidgeotto', height:'1', types:['flying','normal']},
+        {name:'Venomoth', height:'1.5', types:['bug','poison']},
+        {name:'Sandshrew', height:'0.6', types:['ground']}
 ];
+
+// iterates over every pokemon
+for (let i=0; i<pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `);
+    
+    // highlights the special pokemon (taller than 1)
+    if (pokemonList[i].height>1) {
+        document.write (' - Wow, that\'s big! ')
+    }
+}
