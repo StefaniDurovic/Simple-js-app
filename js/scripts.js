dd6813a25c5bdc129b97ba1a0c607debcde5a9f6
StefaniDurovic <95530637+StefaniDurovic@users.noreply.github.com>
2022-09-28 00:15:18 +0200

    applied forEach method to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ let  pokemonList = [
         {name:'Sandshrew', height:'0.6', types:['ground']}
 ];
 
+/* for loop function
 // iterates over every pokemon
 for (let i=0; i<pokemonList.length; i++) {
     document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `);
@@ -13,4 +14,23 @@ for (let i=0; i<pokemonList.length; i++) {
     if (pokemonList[i].height>1) {
         document.write (' - Wow, that\'s big! ')
     }
-}
+
+    document.write('</br>') 
+}*/
+
+
+// forEach method that iterates through the pokemonList array
+pokemonList.forEach(function(pokemon) {
+    document.write(`${pokemon.name} (height: ${pokemon.height}) `);
+    
+    // highlights the special pokemon (taller than 1)
+    if (pokemon.height>1) {
+        document.write(' - Wow, that\'s big! ');
+    }
+
+    document.write('</br>');
+});
+
+
+
+    
